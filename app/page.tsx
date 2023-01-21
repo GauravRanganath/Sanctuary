d282382b3a64ba70c189605892beb1e0ed66bfbe
Gaurav Ranganath <gauravranganath@Gauravs-MacBook-Pro.local>
2023-01-20 23:22:00 -0500

    use mern stack

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import '@root/global.scss';
-
-import DefaultLayout from '@components/DefaultLayout';
-
-export default async function Page(props) {
-  return <DefaultLayout>Hello World</DefaultLayout>;
-}
--- components/DefaultLayout.tsx
+++
@@ -1,7 +0,0 @@
-import styles from '@components/DefaultLayout.module.scss';
-
-import * as React from 'react';
-
-export default function App(props) {
-  return <div className={styles.body}>{props.children}</div>;
-}
