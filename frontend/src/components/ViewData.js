4500be43d384dd3e697c50715ce6fbc697f6972f
Cassey Shao <casseys0000@gmail.com>
2023-01-21 15:52:04 -0500

    Clean frontend styling

--- frontend/src/components/ViewData.js
+++ frontend/src/components/ViewData.js
@@ -6,7 +6,6 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { width } from "@mui/system";
 
 function ViewData() {
   // Retrieve Estuary files
