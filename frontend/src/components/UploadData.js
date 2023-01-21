aab2f8337a9f24d2a7606a99065e8aeaea47558e
Gaurav Ranganath <gauravranganath@Gauravs-MacBook-Pro.local>
2023-01-21 04:34:46 -0500

    add component update

--- frontend/src/components/UploadData.js
+++ frontend/src/components/UploadData.js
@@ -1,10 +1,13 @@
+import NavBar from "./Navbar"
+import BasicButtons from "./Button";
+
 function UploadData() {
-    return (
-      <div>
-        <h1>Upload Data</h1>
-      </div>
-    );
-  }
-  
-  export default UploadData;
-  
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h1>Upload Data</h1>
+    </div>
+  );
+}
+
+export default UploadData;
