23804744eb3e3e7b1fd93a39d62f69fa5c57de32
Cassey Shao <casseys0000@gmail.com>
2023-01-21 12:19:32 -0500

    Add buttons on home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,7 @@
 import NavBar from "./Navbar";
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -11,6 +13,15 @@ function Home() {
         <br></br>
         <h1>Better data.</h1>
         <h1>Better treatment.</h1>
+        <br></br>
+        <div>
+          <Link to="/upload-data">
+            <Button variant="primary" size="lg">Upload Data</Button>
+          </Link>
+          <Link to="/view-data">
+            <Button variant="primary" size="lg">View Data</Button>
+          </Link>
+        </div>
       </Container>
     </div>
   );
