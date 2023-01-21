80380158801f7e5a5d2581e53c80cce6abddcb15
Ramakrishna Natarajan <nkrama99@gmail.com>
2023-01-21 15:51:55 -0500

    update create script

--- data_scripts/create_files.py
+++ data_scripts/create_files.py
@@ -1,16 +1,18 @@
-fileNames = ['./data/lung_cancer-male-10-caucasian.txt',
-             './data/lung_cancer-male-3-caucasian.txt',
-             './data/lung_cancer-male-70-caucasian.txt',
-             './data/lung_cancer-male-57-caucasian.txt',
-             './data/lung_cancer-male-59-hispanic.txt',
-             './data/lung_cancer-female-65-african_american.txt',
-             './data/lung_cancer-female-60-asian.txt',
-             './data/lung_cancer-female-45-hispanic.txt',
-             './data/lung_cancer-female-34-asian.txt',
-             './data/lung_cancer-female-23-asian.txt']
+fileNames = ['lung_cancer-male-10-caucasian.txt',
+             'lung_cancer-male-3-caucasian.txt',
+             'lung_cancer-male-70-caucasian.txt',
+             'lung_cancer-male-57-caucasian.txt',
+             'lung_cancer-male-59-hispanic.txt',
+             'lung_cancer-female-65-african_american.txt',
+             'lung_cancer-female-60-asian.txt',
+             'lung_cancer-female-45-hispanic.txt',
+             'lung_cancer-female-34-asian.txt',
+             'lung_cancer-female-23-asian.txt',
+             ]
 
 for x in fileNames:
-    print("creating:", x)
-    f = open(x, "w")
+    fileName = './data/' + x
+    print("creating:", fileName)
+    f = open(fileName, "w")
     f.write(x + " CONTENT: test")
     f.close()
