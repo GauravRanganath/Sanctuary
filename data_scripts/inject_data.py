656ce4d02e7a86ee792c59493d9b508d6ef47f54
Ramakrishna Natarajan <nkrama99@gmail.com>
2023-01-21 18:49:31 -0500

    injection code updates

--- data_scripts/inject_data.py
+++ data_scripts/inject_data.py
@@ -1,15 +1,11 @@
 import requests
 
-fileNames = ['lung_cancer-male-10-caucasian.txt',
-             'lung_cancer-male-3-caucasian.txt',
-             'lung_cancer-male-70-caucasian.txt',
-             'lung_cancer-male-57-caucasian.txt',
-             'lung_cancer-male-59-hispanic.txt',
-             'lung_cancer-female-65-african_american.txt',
-             'lung_cancer-female-60-asian.txt',
-             'lung_cancer-female-45-hispanic.txt',
-             'lung_cancer-female-34-asian.txt',
-             'lung_cancer-female-23-asian.txt',
+fileNames = ['rama15-rama11-rama12-rama13.txt',
+             'rama15-rama11-rama12-rama13.txt',
+             'rama15-rama11-rama12-rama13.txt',
+             'rama15-rama11-rama12-rama13.txt',
+             'rama15-rama11-rama12-rama13.txt',
+             'rama15-rama11-rama12-rama13.txt',
              'rama15-rama11-rama12-rama13.txt'
              ]
 
@@ -20,6 +16,7 @@ classifiication_dict = {
     "lung_cancer": "a6185f93-139d-4f0d-ba13-d54016d66c6d",
     "osteoporosis": "45f0ef77-ce0e-44a0-8ec6-17b9dae601f2",
     "female": "0e215c75-ea14-49e2-80c3-1b5eba937e49",
+    "rama15": "93274a38-e0e9-4299-a48a-1502f6def8a1",
     "collection1": "ef26a93d-1bb0-45a3-85d6-2223e923a153",
     "rama2": "9531a715-d8cb-4808-a490-18056108a8a6",
     "african_american": "1d9bb3eb-ecd0-4f4c-a082-3be829a96e4d",
@@ -37,7 +34,11 @@ classifiication_dict = {
     "a2": "9f5f0dc7-8f39-4260-bc2e-7315615b8f55",
     "a3": "b6c9ba35-0ac6-4250-a80f-544bedecb01d",
     "a4": "b39791d8-5029-42a0-8119-9cf0ed008642",
-    "a5": "87ee9836-0d00-4eca-9461-7756e320f61f"
+    "a5": "87ee9836-0d00-4eca-9461-7756e320f61f",
+    "rama11": "762a4a41-b397-4698-9f82-b75a1f9a8b9a",
+    "rama12": "e48c0f08-1b03-4009-b736-eab86529d832",
+    "rama13": "b608d4a2-3c3f-4908-b22d-82fdf5aec48f",
+    "rama14": "235ec7e7-ae8d-4efc-bde8-36eea92c9e4b"
 }
 
 url = "https://api.estuary.tech/content/add"
@@ -69,7 +70,8 @@ estuaryId = res.json()["estuaryId"]
 print("estuaryId", estuaryId)
 
 # add to collections
-url_disease = "https://api.estuary.tech/collections/" + classifiication_dict[disease]
+url_disease = "https://api.estuary.tech/collections/" + \
+    classifiication_dict[disease]
 url_sex = "https://api.estuary.tech/collections/" + classifiication_dict[sex]
 url_age = "https://api.estuary.tech/collections/" + classifiication_dict[age]
 url_race = "https://api.estuary.tech/collections/" + classifiication_dict[race]
