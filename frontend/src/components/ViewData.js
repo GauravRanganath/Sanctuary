7940462d2cd93ebe9e0228a1d19f011e881f95df
Cassey Shao <casseys0000@gmail.com>
2023-01-21 02:08:05 -0500

    Add Estuary files retrieval call

--- frontend/src/components/ViewData.js
+++ frontend/src/components/ViewData.js
@@ -1,4 +1,20 @@
 function ViewData() {
+
+  // Retrieve Estuary files
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "application/json");
+  myHeaders.append("Authorization", "Bearer EST0eae79ed-3ff2-414a-a139-3f5564be0344ARY"); // Put the Bearer key in a variable
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.estuary.tech/content/list", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
     return (
       <div>
         <h1>View Data</h1>
