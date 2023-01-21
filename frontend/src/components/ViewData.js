5460534643f934523620f9250f29e42a0deae82f
Gaurav Ranganath <gauravranganath@Gauravs-MacBook-Pro.local>
2023-01-21 02:23:44 -0500

    add estuary code

--- frontend/src/components/ViewData.js
+++ frontend/src/components/ViewData.js
@@ -1,25 +1,32 @@
-function ViewData() {
+import React, { useState } from 'react';
 
+function ViewData() {
   // Retrieve Estuary files
+  const [test, setTest] = useState([]);
+
   var myHeaders = new Headers();
   myHeaders.append("Accept", "application/json");
-  myHeaders.append("Authorization", "Bearer EST0eae79ed-3ff2-414a-a139-3f5564be0344ARY"); // Put the Bearer key in a variable
+  myHeaders.append(
+    "Authorization",
+    "Bearer EST0eae79ed-3ff2-414a-a139-3f5564be0344ARY"
+  ); // Put the Bearer key in a variable
 
   var requestOptions = {
-    method: 'GET',
+    method: "GET",
     headers: myHeaders,
-    redirect: 'follow'
+    redirect: "follow",
   };
 
   fetch("https://api.estuary.tech/content/list", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-    return (
-      <div>
-        <h1>View Data</h1>
-      </div>
-    );
-  }
-  
-  export default ViewData;
+    .then((response) => response.text())
+    .then((result) => setTest(result))
+    .catch((error) => console.log("error", error));
+
+  return (
+    <div>
+      <p>{test}</p>
+    </div>
+  );
+}
+
+export default ViewData;
