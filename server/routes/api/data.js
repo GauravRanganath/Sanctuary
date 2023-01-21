b249664d4c78fad7dcd328b17c07adf3ef70e026
Ramakrishna Natarajan <nkrama99@gmail.com>
2023-01-21 13:06:22 -0500

    records endpoint done

--- server/routes/api/data.js
+++ server/routes/api/data.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
+const { json } = require('body-parser');
 dotenv.config();
 const Axios = require('axios').default;
 
@@ -25,7 +26,7 @@ router.get('/records', async (req, res) => {
   const age = req.query.age;
   const race = req.query.race;
 
-  // Step 2: Retrieve all individual collection data
+  // Step 2: Retrieve all individual options (collection names)
   let diseaseData = [];
   let sexData = [];
   let ageData = [];
@@ -49,18 +50,74 @@ router.get('/records', async (req, res) => {
   console.log("age", age, ageData);
   console.log("race", race, raceData);
 
-  // Step 3: Select data to be returned
-  let temp = await Axios.get('https://api.estuary.tech/collections/', config);
-  const collections = temp.data
-  // console.log(collections);
+  let allOptions = [];
+  allOptions = allOptions.concat(diseaseData);
+  allOptions = allOptions.concat(sexData);
+  allOptions = allOptions.concat(ageData);
+  allOptions = allOptions.concat(raceData);
 
-  res.send('returning records!');
+  console.log(allOptions);
+
+  // Step 3: Map collection name to collection key
+  let resp = await Axios.get('https://api.estuary.tech/collections/', config);
+  let allCollections = resp.data;
+  
+  let collectionIds = [];
+  allCollections.forEach(x => {
+    if (allOptions.includes(x.name)) {
+      collectionIds.push(x.uuid);
+    }
+  });
+
+  console.log("collectionIds:", collectionIds); // all collection ids
+
+  // Step 4: create an array or array of CIDS
+  
+  let collectionsAndContents = [];
+  let urls = [];
+  collectionIds.forEach(async x => {
+    let url = "https://api.estuary.tech/collections/" + x;
+    urls.push(url);
+  });
+
+  const requests = urls.map((url) => axios.get(url, config));
+
+  axios.all(requests).then((responses) => {
+    responses.forEach(resp => {
+      // console.log("resp.data:", resp.data);
+      let temp = resp.data;
+      let arrayOfIds = []
+      temp.forEach(x => {
+        arrayOfIds.push(x.cid);
+      });
+      collectionsAndContents.push(arrayOfIds);
+    });
+  }).then(x => {
+    console.log("collectionsAndContents:", collectionsAndContents);
+
+    // Step 5: Intersect and return data
+    function intersect(array1, array2) {
+      return array1.filter(value => array2.includes(value));
+    }
+
+    let result = collectionsAndContents[0];
+    // console.log("first", result);
+
+    collectionsAndContents.forEach(arr => {
+      result = intersect(result, arr);
+      // console.log("in progress", result);
+    });
+
+    console.log("result:", result);
+    res.send(result);
+  });
 });
 
 // -------------------> Leaving code below for reference
 
 // Load Book model
 const Book = require('../../models/Book');
+const { default: axios } = require('axios');
 
 // @route GET api/books
 // @description Get all books
